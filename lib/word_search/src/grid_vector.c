0e44c857b8233160bce62c4704ba3f94caf799f9
someone <someone>
2020-08-07 22:18:44 -0500

    test:GREEN Added implementation for word_search__grid_vector__equals. this method compares two WordSearch__GridVector structures for equality. Returns 1 if the two structures are equal, and 0 otherwise.

--- lib/word_search/src/grid_vector.c
+++ lib/word_search/src/grid_vector.c
@@ -5,5 +5,12 @@ char word_search__grid_vector__equals(
     WordSearch__GridVector const *first,
     WordSearch__GridVector const *second
 ){
+    if( 
+        first->direction == second->direction &&
+        first->magnitude == second->magnitude
+    ){
+        return 1;
+    }
+
     return 0;
 }
