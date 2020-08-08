afa0e7160bea25be13d8b528b7a0af8a7e3decbb
someone <someone>
2020-08-08 01:09:18 -0500

    test:RED Added test for method word_search__grid_sequence__coordinates_of. This method calculates and returns the coordinates corresponding to the given position of a WordSearch__GridSequence.

--- lib/word_search/src/grid_sequence.c
+++ lib/word_search/src/grid_sequence.c
@@ -1,6 +1,10 @@
 // Internal Includes
 #include "word_search/grid_sequence.h"
 
+char word_search__grid_sequence__coordinates_of( WordSearch__GridSequence* sequence, unsigned long index, WordSearch__GridCoordinates* out_coordinates ){
+    return 0;
+}
+
 char word_search__grid_sequence__equals( 
     WordSearch__GridSequence const *first,
     WordSearch__GridSequence const *second
--- lib/word_search/test/test__word_search__grid_sequence.cpp
+++ lib/word_search/test/test__word_search__grid_sequence.cpp
@@ -30,3 +30,38 @@ TEST_CASE( "word_search__grid_sequence__equals", "[word_search]" ){
     REQUIRE( word_search__grid_sequence__equals( &first, &first ) == 1 );
     REQUIRE( word_search__grid_sequence__equals( &first, &second ) == 0 );
 }
+
+// char word_search__grid_sequence__coordinates_of( WordSearch__GridSequence* sequence, unsigned long index, WordSearch__GridCoordinates* out_coordinates );
+
+TEST_CASE( "word_search__grid_sequence__coordinates_of", "[grid_sequence]" ){
+    WordSearch__GridSequence sequence = {
+        .start = { .row = 0, .column = 0 },
+        .span = { .magnitude = 2, .direction = WordSearch__Direction__East }
+    };
+
+    WordSearch__GridCoordinates coordinates;
+    WordSearch__GridCoordinates expected_coordinates;
+
+    REQUIRE( word_search__grid_sequence__coordinates_of( &sequence, 0, &coordinates ) );
+    REQUIRE( word_search__grid_coordinates__equals( &sequence.start, &coordinates ) );
+
+    expected_coordinates = { .row = 0, .column = 1 };
+    REQUIRE( word_search__grid_sequence__coordinates_of( &sequence, 1, &coordinates ) );
+    REQUIRE( word_search__grid_coordinates__equals( &coordinates, &expected_coordinates ) );
+
+    sequence.span.direction = WordSearch__Direction__SouthEast;
+    expected_coordinates = (WordSearch__GridCoordinates) { .row = 1, .column = 1 };
+    REQUIRE( word_search__grid_sequence__coordinates_of( &sequence, 1, &coordinates ) );
+    REQUIRE( word_search__grid_coordinates__equals( &coordinates, &expected_coordinates ) );
+    
+    sequence.span.direction = WordSearch__Direction__South;
+    expected_coordinates = (WordSearch__GridCoordinates) { .row = 1, .column = 0 };
+    REQUIRE( word_search__grid_sequence__coordinates_of( &sequence, 1, &coordinates ) );
+    REQUIRE( word_search__grid_coordinates__equals( &coordinates, &expected_coordinates ) );
+
+    sequence.span.direction = WordSearch__Direction__SouthWest;
+    expected_coordinates = (WordSearch__GridCoordinates) { .row = 1, .column = -1 };
+    REQUIRE( word_search__grid_sequence__coordinates_of( &sequence, 1, &coordinates ) );
+
+    REQUIRE_FALSE( word_search__grid_sequence__coordinates_of( &sequence, 2, &coordinates ) );
+}
