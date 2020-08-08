e6de6b7447dd3dc6d862410d14fb1e67c1193971
someone <someone>
2020-08-08 00:29:30 -0500

    test:GREEN Added implementation for word_search__grid__contains. This method verifies whether the given WordSearch__GridCoordinates fall within the WordSearch__Grid.  Returns 1 if the coordinates are within the grid, and 0 otherwise.

--- lib/word_search/src/grid.c
+++ lib/word_search/src/grid.c
@@ -2,5 +2,14 @@
 #include "word_search/grid.h"
 
 char word_search__grid__contains( WordSearch__Grid const *grid, WordSearch__GridCoordinates const *coordinates ){
+    if( 
+        coordinates->row >= 0 &&
+        coordinates->row < grid->height &&
+        coordinates->column >= 0 &&
+        coordinates->column < grid->width
+    ){
+        return 1;
+    }
+
     return 0;
 }
