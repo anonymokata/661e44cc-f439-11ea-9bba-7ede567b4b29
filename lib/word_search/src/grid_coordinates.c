b27d8f13420cb859fb5e00e13dc6f0c598559011
someone <someone>
2020-08-07 21:57:13 -0500

    test:GREEN Added implementation for word_search__grid_coordinates__equals. This method compares two WordSearch__GridCoordinates for equality. Returns 1 if the two structures are equal, and 0 otherwise.

--- lib/word_search/src/grid_coordinates.c
+++ lib/word_search/src/grid_coordinates.c
@@ -5,5 +5,12 @@ char word_search__grid_coordinates__equals(
     WordSearch__GridCoordinates const *first,
     WordSearch__GridCoordinates const *second
 ){
+    if( 
+        first->row == second->row &&
+        first->column == second->column 
+    ){
+        return 1;
+    }
+
     return 0;
 }
