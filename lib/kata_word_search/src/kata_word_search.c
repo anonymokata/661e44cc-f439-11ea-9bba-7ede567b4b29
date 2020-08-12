f65708b029a504566c2252039a179813bf636ead
someone <someone>
2020-08-12 13:25:31 -0500

    test:GREEN Added implementation for method kata_word_search__find_word. This method searches the grid in the given direction for the given word. 1 if the word was found, and 0 otherwise.

--- lib/kata_word_search/src/kata_word_search.c
+++ lib/kata_word_search/src/kata_word_search.c
@@ -6,5 +6,31 @@ char kata_word_search__find_word(
     WordSearch__Direction direction, 
     WordSearch__GridSequence* out_sequence 
 ){
+    if( out_sequence == NULL ){
+        return 0;
+    }
+
+    for( unsigned long row_index = 0; row_index < grid->height; row_index++ ){
+
+        for( unsigned long column_index = 0; column_index < grid->width; column_index++ ){
+
+            WordSearch__GridSequence current_sequence = {
+                .start = {
+                    .row = row_index,
+                    .column = column_index
+                },
+                .span = {
+                    .magnitude = word->length,
+                    .direction = direction
+                }
+            };
+
+            if( word_search__grid__sequence_matches_word( grid, &current_sequence, word ) ){
+                *out_sequence = current_sequence;
+                return 1;
+            }
+        }        
+    }
+
     return 0;
 }
